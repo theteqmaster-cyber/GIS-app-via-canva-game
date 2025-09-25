82fd73432e932ba400f22a3a36822ae8821d3687
mphatic <theteqmaster@gmail.com>
2025-09-25 22:55:16 +0200

    Update game.js

--- game.js
+++ game.js
@@ -44,7 +44,7 @@ const ROADBLOCK_WIDTH = 42;
 const ROADBLOCK_HEIGHT = 32;
 const ROADBLOCK_COLOR = "#e74c3c";
 // Increased frequency value so roadblocks are farther apart:
-const ROADBLOCK_FREQ = 1200; // was 520, now farther apart
+const ROADBLOCK_FREQ = 3000; // was 520, now farther apart
 const ROADBLOCK_WAIT = 4000; // ms
 
 let roadblocks = [];
